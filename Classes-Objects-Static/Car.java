b5c946e78957e6c42b1a59b65f250060f883e5fa
Kirk Harland <k.harland@leeds.ac.uk>
2013-06-26 09:18:49 +0100

    Create Car.java
    The final state of the Car class for the Classes, Objects and Static practical

---
+++ Classes-Objects-Static/Car.java
@@ -0,0 +1,16 @@
+//class declaration for our first class Car.
+public class Car {
+
+  //declaring a double for storing the default engine size
+	public static double defaultEngineSize = 1.0;
+	
+	//declare the String for storing the car colour
+	public static String defaultCarColour = "White";
+
+	//declaring the double for storing the engine size
+	public double engineSize = defaultEngineSize;
+	
+	//declare the String for storing the car colour
+	public String carColour = defaultCarColour;
+
+}
